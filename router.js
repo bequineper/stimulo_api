093a0d182cc5af42d346ebc31254231744a0961b
bequineper <quineperequineper@gmail.com>
2025-04-09 17:42:21 -0300

    add new user/file routes

--- router.js
+++ router.js
@@ -1,10 +1,30 @@
-import {Router} from `express`;
-import {loginUser, registerUser} from './controllers/userController.js';
+import Router from 'express';
+import {
+  loginUser,
+  registerUser, 
+  getUserProfile,
+  adminGetUser
+} from './controllers/userController.js';
+import {
+    createFile,
+    deleteFile,
+    getFile,
+    listFiles
+  } from "./controllers/fileController.js";
+import {authenticateToken} from "./middleware/auth.js";
 
 const router = Router();
 
 router.post ('/register', registerUser);
 router.post ('/login', loginUser);
+router.get  ('/admin/:id', adminGetUser);
+
+router.get(`/me`, authenticateToken, getUserProfile)
+
+router.post('/file', authenticateToken, createFile);
+router.get('/file/:name', authenticateToken, listFiles);
+router.get('/file/:id', authenticateToken, getFile);
+router.delete('/file/:id', authenticateToken, deleteFile);
 
 export default router;
 
