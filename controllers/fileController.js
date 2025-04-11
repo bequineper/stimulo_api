edccd3df881764b40db71d346d39bd03fb11f433
bequineper <quineperequineper@gmail.com>
2025-04-11 18:21:12 -0300

    Add/Fix all file controllers

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,18 +1,23 @@
+import { decrypt } from 'dotenv';
 import db from '../db.js';
+import { encrypt } from '../utils/cryptoUtils.js';
 
 export async function createFile(req,res) {
-    const {name} = req.body;
+    const {name, content} = req.body;
     const user_id = req.user.id;
+
+    if (!name.endsWith('.md')) {
+        name += '.md';
+    }
     
     try {
-        const [id] = await db('file').insert({name, user_id});
+        const encryptedContent = encrypt(content);
+        const [id] = await db('file').insert({name, user_id, content: encryptedContent});
         res.status(201).json({id, name});
     } catch (err) {
         console.error(err);
         res.status(500).json({Message: 'Erro ao criar arquivo' });
-        
     }
-    
 }
 
 export async function listFiles(req,res) {
@@ -23,27 +28,34 @@ export async function listFiles(req,res) {
 
         const files = await db('file')
         .where({user_id: userId})
-        .select('id','name');
+        .select('id','name','content');
         res.status(200).json({files});
     } catch (err) {
         console.error(err);
         res.status(500).json({message: 'Erro ao buscar arquivos'});
     }   
-}
-    
+}  
     
 export async function getFile(req,res) {
     const {id} = req.params;
+    
     const user_id = req.user.id;
     
     try{
-        const file = await db('file').where({id, user_id});
+        const file = await db('file')
+        .where({id, user_id})
+        .select('id','name','content')
+        .first();
+        
         if(!file)  return res.status(404).json({message: `Arquivo não encontrado`});
-        res.status(200).json(file); 
+        
+
+        const decryptedContent = decrypt(file.content);
+        res.status(200).json({...file, content: decryptedContent}); 
+
     }catch(err){
         console.error(err);
         res.status(500).json({message: `Erro ao buscar arquivo`});
-        
     }
 }
 
@@ -63,3 +75,63 @@ export async function deleteFile(req,res){
     }
     
 }
+
+export async function updateFileContent(req,res) {
+    const { id } = req.params;
+    const { content } = req.body;
+    const user_id = req.user.id;
+
+    try {
+        const encryptedContent = encrypt(content);
+
+        const updated = await db('file')
+        .where({id, user_id})
+        .update({content: encryptedContent});
+
+        
+        if(!updated){
+            return res.status(404).json({message: `Arquivo não encontrado`});
+
+        }
+
+        res.status(200).json({message: `Conteúdo atualizado com sucesso`});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: `Erro ao atualizar conteúdo`});
+    }
+}
+
+export async function updateFileName(req,res){
+    const {id} = req.params;
+    const user_id = req.user.id;
+    const {name} = req.body;
+
+
+    if (!name.endsWith('.md')) {
+        name += '.md';
+      }
+
+    try {
+        const updated = await db('file')
+        .where({id, user_id})
+        .update({name});
+
+        if(!updated){
+            return res.status(404).json({message: `Arquivo não encontrado`});
+        }
+
+        res.status(200).json({message: 'Nome do arquivo atualizado com sucesso'});
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: `Impossivel atualizar o nome do arquivo`});
+        
+    }
+
+
+
+
+    
+}
+
+
